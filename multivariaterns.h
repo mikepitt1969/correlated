539ca11afdf1d52b9f1e1065acdce8ca23ebdcca
mikepitt1969 <36702432+mikepitt1969@users.noreply.github.com>
2018-02-21 15:09:48 +0000

    Delete multivariaterns.h

--- multivariaterns.h
+++
@@ -1,7 +0,0 @@
-
-ranmultT(const vmu, const mSig, const dof);
-	 
-
-/* standard (non-copular) multivariate t-density  */
-logdensmultT(const x, const vmu, const mSig, const dof);
-densmultT(const x, const vmu, const mSiginv, const dof);
